2ddf314d984c40e1c1db9d98594c086e52572e57
Nataly <natalycarvalho.dev@gmail.com>
2022-08-12 12:50:56 -0300

    Add Service e Controller de usuario

--- ProjetoIntegrador/src/main/java/com/generation/ProjetoIntegrador/repository/UsuarioRepository.java
+++ ProjetoIntegrador/src/main/java/com/generation/ProjetoIntegrador/repository/UsuarioRepository.java
@@ -3,11 +3,12 @@ package com.generation.ProjetoIntegrador.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.ProjetoIntegrador.model.UsuarioModel;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
-	public Optional<UsuarioModel> findByUsuario(String usuario);
+	public Optional<UsuarioModel> findByUsuario(@Param ("usuario")String usuario);
 }
